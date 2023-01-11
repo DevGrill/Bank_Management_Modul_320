65aefb37af0484e55f1f077e27431a1fcf3928f9
Grilly <cyril.kurmann@gmail.com>
2023-01-11 20:46:22 +0100

    Junit tests for BankManager

---
+++ src/main/java/me/devgrill/BankManager/BankManagerTest.java
@@ -0,0 +1,30 @@
+package me.devgrill.BankManager;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BankManagerTest {
+
+    @Test()
+    void addAccount() {
+        BankManager bankManager = new BankManager();
+        Assertions.assertDoesNotThrow(() -> bankManager.addAccount("Test"));
+        bankManager.removeAccount("Test");
+    }
+
+    @Test
+    void checkAccount() {
+        BankManager bankManager = new BankManager();
+        bankManager.addAccount("Test");
+        Assertions.assertTrue(bankManager.checkAccount("Test"));
+        bankManager.removeAccount("Test");
+        Assertions.assertFalse(bankManager.checkAccount("Test"));
+    }
+
+    @Test
+    void removeAccount() {
+        BankManager bankManager = new BankManager();
+        bankManager.addAccount("Test");
+        Assertions.assertDoesNotThrow(() -> bankManager.removeAccount("Test"));
+    }
+}
