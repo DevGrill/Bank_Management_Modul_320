9b8770a41f63be2e3b995d8d4e13c739adc5490d
Grilly <cyril.kurmann@gmail.com>
2023-01-11 21:20:12 +0100

    Removed unused import

--- src/main/java/me/devgrill/AccountManager/AccountManagerTest.java
+++ src/main/java/me/devgrill/AccountManager/AccountManagerTest.java
@@ -3,8 +3,6 @@ package me.devgrill.AccountManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class AccountManagerTest {
 
     @Test
