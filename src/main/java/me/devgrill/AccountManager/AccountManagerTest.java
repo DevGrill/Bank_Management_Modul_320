f8fa75453ce1ae0dcafe82b3c82427a40567cc25
Grilly <cyril.kurmann@gmail.com>
2023-01-11 20:53:31 +0100

    Added AccountManagerTest

---
+++ src/main/java/me/devgrill/AccountManager/AccountManagerTest.java
@@ -0,0 +1,20 @@
+package me.devgrill.AccountManager;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountManagerTest {
+
+    @Test
+    void getBalance() {
+    }
+
+    @Test
+    void addMoney() {
+    }
+
+    @Test
+    void removeMoney() {
+    }
+}
